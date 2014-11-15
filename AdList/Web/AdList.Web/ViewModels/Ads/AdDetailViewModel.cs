d04fcf6c866a433070f1aaa955644fa7f4acb2fb
neutrino <jrusev@gmail.com>
2014-11-15 16:45:21 +0200

    ad detail page

--- AdList/Web/AdList.Web/ViewModels/Ads/AdDetailViewModel.cs
+++ AdList/Web/AdList.Web/ViewModels/Ads/AdDetailViewModel.cs
@@ -1,9 +1,9 @@
-﻿using AdList.Data.Models;
-using AdList.Web.Infrastructure.Mapping;
+﻿using AdList.Web.Infrastructure.Mapping;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+
+using AdList.Data.Models;
+using AdList.Web.ViewModels.User;
+using System.ComponentModel.DataAnnotations;
 
 namespace AdList.Web.ViewModels.Ads
 {
@@ -12,5 +12,19 @@ namespace AdList.Web.ViewModels.Ads
         public string Title { get; set; }
 
         public string Description { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MMM dd, yyy}")]
+        public DateTime CreatedOn { get; set; }
+
+        public virtual UserProfileViewModel Author { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public bool Featured { get; set; }
     }
 }

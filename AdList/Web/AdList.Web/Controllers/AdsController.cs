5a142049b505e6c70ad087838df47e7f749c2a19
neutrino <jrusev@gmail.com>
2014-11-16 18:20:38 +0200

    menu for administration

--- AdList/Web/AdList.Web/Controllers/AdsController.cs
+++ AdList/Web/AdList.Web/Controllers/AdsController.cs
@@ -3,7 +3,6 @@
     using AdList.Data.Common.Repository;
     using AdList.Data.Models;
     using AdList.Web.InputModels.Ads;
-    using System;
     using System.Linq;
     using System.Web.Mvc;
     using AdList.Web.ViewModels.Ads;
@@ -11,7 +10,6 @@
     using AdList.Web.Infrastructure;
     using Microsoft.AspNet.Identity;
     using AdList.Web.ViewModels.Home;
-    using PagedList;
 
     public class AdsController : AdsPagingControllerBase
     {
@@ -39,6 +37,7 @@
             return this.View(model);
         }
 
+        [Authorize(Roles = "Administrator")]
         public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
             var allAds = this.ads.All().Project().To<AdDetailViewModel>();
@@ -107,5 +106,72 @@
 
             return this.View(input);
         }
+
+        #region Adminstrion
+
+        // GET: Admin/Edit/5
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            var ad = this.ads.All().FirstOrDefault(x => x.Id == id);
+            if (ad == null)
+            {
+                return this.HttpNotFound("No ad with such id!");
+            }
+
+            var model = new AdInputModel()
+            {
+                Title = ad.Title,
+                Description = ad.Description,
+                Price = ad.Price,
+                ImageUrl = ad.ImageUrl,
+                CategoryId = ad.CategoryId
+            };
+            model.CategoryOptions = this.categories.All().OrderBy(x => x.Name);
+
+            return this.View(model);
+        }
+
+        // POST: Admin/Edit/5
+        [Authorize]
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Delete/5
+        [Authorize]
+        public ActionResult Delete(int id)
+        {
+            return RedirectToAction("Index");
+        }
+
+        // POST: Admin/Delete/5
+        [Authorize]
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        #endregion
     }
 }

d0acaeabc1a84408836849efc72cc36ad157e59d
neutrino <jrusev@gmail.com>
2014-11-18 11:48:22 +0200

    enabled edit for users

--- AdList/AdList.Web/Controllers/AdsController.cs
+++ AdList/AdList.Web/Controllers/AdsController.cs
@@ -10,6 +10,7 @@
     using Microsoft.AspNet.Identity;
     using AdList.Web.ViewModels.Home;
     using AdList.Data.UnitOfWork;
+    using System.Net;
 
     public class AdsController : AdsPagingControllerBase
     {
@@ -43,7 +44,7 @@
             return View(stats.ToList());
         }
 
-        // /ads/details/7
+        // GET: /Ads/Details/7
         public ActionResult Details(int id, int page = 1)
         {
             var adViewModel = this.Data.Ads.All().Where(x => x.Id == id)
@@ -57,6 +58,7 @@
             return View(adViewModel);
         }
 
+        // GET: /Ads/Create
         [HttpGet]
         [Authorize]
         public ActionResult Create()
@@ -66,6 +68,7 @@
             return this.View(model);
         }
 
+        // POST: /Ads/Create
         [HttpPost]
         [Authorize]
         [ValidateAntiForgeryToken]
@@ -91,6 +94,85 @@
             }
 
             return this.View(input);
+        }
+
+        // GET: /Ads/Edit/5
+        [Authorize]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var ad =
+                this.Data.Ads
+                .All()
+                .Project().To<AdInputModel>()
+                .Where(x => x.Id == id)
+                .FirstOrDefault();
+
+            if (ad == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            if (ad.AuthorId != this.CurrentUser.Id && !this.User.IsInRole(AdList.Data.Models.User.AdminRole))
+            {
+                return this.RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryOptions = this.Data.Categories.All().OrderBy(x => x.Name);
+
+            return this.View(ad);
+        }
+
+        // POST: /Ads/Edit/5
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(AdInputModel input)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.View(input);
+            }
+
+            var adFromDb = this.Data.Ads.Find(input.Id);
+
+            if (adFromDb.AuthorId != this.CurrentUser.Id && !User.IsInRole(AdList.Data.Models.User.AdminRole))
+            {
+                return this.RedirectToAction("Index");
+            }
+
+            adFromDb.Title = input.Title;
+            adFromDb.Price = input.Price;
+            adFromDb.Description = input.Description;
+            adFromDb.CategoryId = input.CategoryId;
+            adFromDb.ImageUrl = input.ImageUrl;
+
+            this.Data.Ads.Update(adFromDb);
+            this.Data.SaveChanges();
+
+            return this.RedirectToAction("MyAds","User");
+        }
+
+        // POST: /Ads/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ad ad = this.Data.Ads.Find(id);
+
+            if (ad.AuthorId != this.CurrentUser.Id && !User.IsInRole(AdList.Data.Models.User.AdminRole))
+            {
+                return this.RedirectToAction("Index");
+            }
+
+            this.Data.Ads.Delete(ad);
+            this.Data.SaveChanges();
+
+            return this.RedirectToAction("Index");
         }
     }
 }
